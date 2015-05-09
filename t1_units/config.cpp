5e2be69de13968b8ee4e787d200c0b6b55651d3a
MaHuJa <github@mahuja.net>
2015-05-09 03:47:18 +0200

    Removed dependency listing for navipad

--- t1_units/config.cpp
+++ t1_units/config.cpp
@@ -20,7 +20,7 @@ class CfgPatches
 		weapons[] = {"T1_V_PlateCarrier2_Standard","T1_U_Pilot_Night","T1_U_Pilot_Camo","T1_U_Pilot_BlackCamo","T1_U_Pilot_Black","T1_U_Pilot_Standard","T1_U_Instructor_Polo_Orange","T1_U_Instructor_Polo_Black","T1_U_Sniper_Standard","T1_U_Diver_Standard","T1_U_Squad_Standard","T1_rhs_m4a1_m320_Hamr_pointer_F","T1_rhs_weap_m4a1_grip_Hamr_pointer_F","T1_LMG_BWA3_MG5_Hamr_pointer_F","T1_LMG_hlc_M60E4_Hamr","T1_srifle_LRR_SOS_pointer_F","T1_srifle_EBR_DMS_pointer_F","BWA3_G28_Assault_SOS_pointer_sd","T1_BWA3_MP7_LLM01_snds_F","T1_hgun_Pistol_heavy_01_snds_F"};
 		magazines[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"hlcweapons_m60e4","BWA3_Weapons","BWA3_Weapons_t1fix","TIER1_LAW","ace_medical","BWA3_NaviPad","BWA3_Kestrel","rhsusf_c_weapons"};
+		requiredAddons[] = {"hlcweapons_m60e4","BWA3_Weapons","BWA3_Weapons_t1fix","TIER1_LAW","ace_medical","BWA3_Kestrel","rhsusf_c_weapons"};
 	};
 };
 class CfgFactionClasses

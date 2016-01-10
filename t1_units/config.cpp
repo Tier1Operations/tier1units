75bc4756096c406f3e3302f5db2b1eb83a7d53d5
MaHuJa <github@mahuja.net>
2016-01-10 23:54:24 +0100

    CLS Update

--- t1_units/config.cpp
+++ t1_units/config.cpp
@@ -598,14 +598,19 @@ class CfgVehicles
 		scope = 1;
 		class TransportItems
 		{
-			class _xx_ACE_quikclot
+			class Bandage1
 			{
-				count = 45;
-				name = "ACE_quikclot";
+				count = 30;
+				name = "ACE_packingBandage";
+			};
+			class Bandage2
+			{
+				count = 10;
+				name = "ACE_elasticBandage";
 			};
 			class _xx_ACE_morphine
 			{
-				count = 10;
+				count = 20;
 				name = "ACE_morphine";
 			};
 			class _xx_ACE_epinephrine
@@ -616,12 +621,17 @@ class CfgVehicles
 			class _xx_ACE_bloodIV_250
 			{
 				count = 10;
-				name = "ACE_bloodIV_250";
+				name = "ACE_salineIV_250";
 			};
-			class _xx_rhs_goggles_clear
+			class MagicalAidKit
 			{
 				count = 1;
-				name = "rhs_goggles_clear";
+				name = "ACE_personalAidKit";
+			};
+			class Tourniquet
+			{
+				count = 4;
+				name = "ACE_tourniquet";
 			};
 		};
 		class TransportMagazines

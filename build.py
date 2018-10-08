4ce3bf228c744d5acd1cf8bbb6bf01d30c743f5f
MaHuJa <github@mahuja.net>
2018-10-08 13:45:40 +0200

    Automatic mod builder

---
+++ build.py
@@ -0,0 +1,41 @@
+#
+toolsfolder = 'D:\\SteamLibrary\\SteamApps\\common\\Arma 3 Tools\\';
+outfolder = 'D:\\@tier1ops\\addons\\';
+privkey = 'D:\\Nextcloud\\tier1ops.biprivatekey';
+
+
+#specific tools
+filebank = toolsfolder + 'FileBank\\Filebank.exe';
+dssign = toolsfolder + 'DSSignFile\\dssignfile.exe';
+binmake = toolsfolder + 'BinMake\\binmake.exe';
+
+import glob;
+pbos = glob.glob('t1_*');
+
+import subprocess;
+#    check_call(*popenargs, **kwargs)
+#        Run command with arguments.  Wait for command to complete.  If
+#        the exit code was zero then return, otherwise raise
+#        CalledProcessError.  The CalledProcessError object will have the
+#        return code in the returncode attribute.
+#        The arguments are the same as for the call function.  Example:
+#
+#        check_call(["ls", "-l"])
+
+for pbo in pbos:
+	print(pbo);
+	subprocess.check_call([
+		filebank,
+		'-dst',
+		outfolder,
+		pbo
+	]);
+	subprocess.check_call([
+		dssign,
+		privkey,
+		outfolder + pbo + '.pbo'
+	]);
+
+
+
+

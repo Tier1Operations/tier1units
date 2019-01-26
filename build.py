b94f17ae7e75c0ab99ef1812aee5739f8e57d26f
MaHuJa <github@mahuja.net>
2019-01-26 14:00:31 +0100

    Build script: Ignore any .folders

--- build.py
+++ build.py
@@ -2,24 +2,25 @@
 toolsfolder = 'E:\\SteamLibrary\\steamapps\\common\\Arma 3 Tools\\';
 outfolder = 'E:\\Stuff\\Arma3_Mod_Stuff\\Build PBOs\\addons\\';
 privkey = 'E:\\Stuff\Arma3_Mod_Stuff\\Key\\tier1ops.biprivatekey';
-ignorefiles = ['.git','.gitignore','binMakeRules.txt','build.py','readme.md'];
+ignorefiles = ['binMakeRules.txt','build.py','readme.md'];
 
+import os;
+import subprocess;
+import glob;
+
+# ignore all . files implicitly
+ignorefiles = ignorefiles + glob.glob(".*");
 
 #specific tools
 filebank = toolsfolder + 'FileBank\\Filebank.exe';
 dssign = toolsfolder + 'DSSignFile\\dssignfile.exe';
 binmake = toolsfolder + 'BinMake\\binmake.exe';
 
-import os;
-import glob;
-pbos = glob.glob('*');
-
 # We're now generating ALL the files, so delete any that may be extra.
 oldfiles = glob.glob(outfolder+"*");
 for file in oldfiles:
 	os.unlink(file);
 
-import subprocess;
 #    check_call(*popenargs, **kwargs)
 #        Run command with arguments.  Wait for command to complete.  If
 #        the exit code was zero then return, otherwise raise
@@ -28,7 +29,7 @@ import subprocess;
 #        The arguments are the same as for the call function.  Example:
 #
 #        check_call(["ls", "-l"])
-
+pbos = glob.glob('*');
 for pbo in pbos:
 	if pbo not in ignorefiles:
 		print(pbo);

721c219af1ba3481a7fc21bbad18b1c35eb237db
BlackAlpha <saschakramarenko@gmail.com>
2020-01-06 19:42:16 +0100

    Bloodfix update for new ACE

--- t1_ace_blood_fix/config.cpp
+++ t1_ace_blood_fix/config.cpp
@@ -6,22 +6,7 @@ class CfgPatches
 		weapons[] = {};
 		magazines[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"ace_medical"};
-	};
-};
-
-
-
-class CfgFunctions
-{
-	class ace_medical
-	{
-		class ace_medical
-		{
-			file = "t1_ace_blood_fix\functions";
-			class getBloodVolumeChange;
-			class handleUnitVitals{};
-		};
+		requiredAddons[] = {"ace_medical_treatment"};
 	};
 };
 
@@ -45,20 +30,17 @@ class CfgWeapons
 
 
 
-class ACE_Medical_Advanced
+class ace_medical_treatment
 {
-	class Treatment
+	class IV
 	{
-		class IV
+		volume = 6000;
+		ratio[] = {};
+		type = "Blood";
+		class BloodIV
 		{
 			volume = 6000;
-			ratio[] = {};
-			type = "Blood";
-			class BloodIV
-			{
-				volume = 6000;
-				ratio[] = {"Plasma",1};
-			};
+			ratio[] = {"Plasma",1};
 		};
 	};
 };

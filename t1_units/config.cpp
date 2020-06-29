2ab8d61016b685688d0879cc2be851ca0d6094ff
BlackAlpha <saschakramarenko@gmail.com>
2020-06-29 19:22:07 +0200

    Vanilla radiobackpack tweaks

--- t1_units/config.cpp
+++ t1_units/config.cpp
@@ -187,6 +187,53 @@ class CfgVehicleClasses
 };
 class CfgVehicles
 {
+	class B_RadioBag_01_base_F;
+	class B_RadioBag_01_mtp_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_wdl_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_tropic_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_black_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_digi_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_eaf_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_hex_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_ghex_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	class B_RadioBag_01_oucamo_F: B_RadioBag_01_base_F
+	{
+		dlc = "Enoch";
+		maximumLoad = 200;
+	};
+	
 	class B_Soldier_base_F;
 	class B_Kitbag_Base;
 	class B_Kitbag_rgr;
